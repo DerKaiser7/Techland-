4e96a0d0bec3b02e8a630543c9c5ac84e14b8c66
DerKaiser7 <44611064+DerKaiser7@users.noreply.github.com>
2019-01-31 11:21:32 +0100

    Add files via upload

---
+++ Tech.php
@@ -0,0 +1,102 @@
+<?php
+	require 'header.php'
+?>
+
+<div class="categories">
+	<div class="row">
+		<div class="col-lg-12" id="category">
+			<h3>Techland Nigerian Forum</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12" id="category">
+				<a href="" title="Techland! Your Number one Nigerian tech Forum">Techland / General:</a> 
+				<a href="">Politics,</a> 
+				<a href="">Crime,</a> 
+				<a href="">Romance,</a>
+				<a href="">Jobs/Vacancies,</a> 
+				<a href="">Career,</a>
+				<a href="">Business,</a> 
+				<a href="">Investment,</a> 
+				<a href="">NYSC,</a> 
+				<a href="">Education,</a>
+				<a href="">Autos,</a> 
+				<a href="">Car Talk,</a>
+				<a href="">Properties,</a> 
+				<a href="">Health,</a> 
+				<a href="">Travel,</a>
+				<a href="">Family,</a> 
+				<a href="">Culture,</a>
+				<a href="">Food,</a> 
+				<a href="">Diaries,</a> 
+				<a href="">Latest Tech News</a> 	
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12">
+			<a href="">Science/Technology:</a> 
+			<a href="">Programming,</a> 
+			<a href="">Databases,</a>
+			<a href="">Webmasters,</a> 
+			<a href="">Computers,</a>
+			<a href="">Phones,</a> 
+			<a href="">Art,</a> 
+			<a href="">Graphics & Video,</a> 
+			<a href="">Tech Innovations,</a>
+			<a href="">Technology Market</a> 
+		</div>
+	</div>
+</div>
+
+<div class="news">
+		<div class="row">
+			<div class="col-lg-12" id="category">
+				<a href="">Featured Links</a> / 
+				<a href="facebook.com/stephenjohnson01">Facebook</a> / 
+				<a href="twitter.com/the_bimss">Twitter</a> /
+				<a href="">How to Advertise</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12"> 
+				<a href="awabike.com">Awabike Launches First bike sharing scheme in Nigeria</a> <br>
+				<a href="">New iPhone features leaks (pictures)</a> <br>
+				<a href="">Appeal Court Can't Stay A Criminal Case - Appeal Court To Onnoghen</a> <br>
+				<a href="">Atiku/Obi On The Candidate: The Interviewer Accused Of Bias As Nigerians React (Pics)</a> <br>
+				<a href="">DJ Cuppy Pictured In The Studio As She Works On A New Song</a>
+			</div>
+		</div>
+</div>
+
+
+<footer>
+	<div class="pagefooter">
+		<div class="row">
+			<div class="col-lg-12">
+				<form action="">
+					<input type="text" name="question" size="64">
+					<button type="submit" name="submit">Search</button>
+				</form>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<a href="">Techland</a> - Copyright © 2005 - 2019 
+				<a href="facebook.com/stephenjohnson01">Falodu Stephen Abimbola</a>. All rights reserved
+				See <a href="">How to Advertise</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				Disclaimer: Every Techland member is solely responsible for anything that he/she posts or uploads on Techland.
+			</div>
+		</div>
+	</div>
+</footer>
+
+
+</body>
+<!-- Latest compiled and minified JavaScript -->
+<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</html>
---
+++ header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" width="400">
+	<title>Techland-Your No 1 hub for Tech related News.</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	<style type="text/css">
+		body{
+			background: #E8ECE0;
+			width: 100%;
+			max-width: 1280px;
+		}
+		.pageheader, .categories, .news, .pagefooter{
+			text-align: center;
+			width: 960px;
+  			border: 1px solid #444;
+  			overflow: hidden;
+  			padding-bottom: 20px;
+  			margin-left: 160px;
+  			margin-top: 25px;
+  			border-radius: 15px;
+  		}
+  		.news, .pagefooter{
+  			padding-top: 5px
+  		}
+  		#category{
+  			border-bottom: 1px solid #444
+  		}
+  		a{
+  			text-decoration: underline;
+  		}
+	</style>
+</head>
+<body>
+<header>
+	<div class="pageheader">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2><a href="">Techland Forum</a></h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				Welcome, <b>Guest</b>: 
+				<a href="">Join Techland</a> /
+				<a href=""><b>LOGIN!</b></a> /
+				<a href="">Trending</a> /
+				<a href="">Recent</a> /
+				<a href="">New</a>
+				<br>
+				<b>Stats:</b> 10 members, 20 topics.
+				<b>Date</b>: Wednesday 30th January 2019 at 16:27
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<form action="">
+					<input type="text" name="question" size="64">
+					<button type="submit" name="submit">Search</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</header> 
